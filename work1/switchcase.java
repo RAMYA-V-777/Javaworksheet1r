225607089d9f87421c1bad5a4f2e187b686dde12
RAMYA-V-777 <68489968+RAMYA-V-777@users.noreply.github.com>
2020-07-28 12:19:08 +0530

    Add files via upload

---
+++ work1/switchcase.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package work11;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author velmurugan
+ */
+public class switchcase {
+    public static void main(String[] args){
+    
+    Scanner obj=new Scanner(System.in);
+    System.out.println("Enter the choice");
+    int c=obj.nextInt();
+    switch(c)
+    {
+        case 1:
+            System.out.println("Valid");
+            break;
+        case 2:
+            System.out.println("Valid");
+                        break;
+
+        case 3:
+            System.out.println("Valid");
+                        break;
+
+        case 4:
+            System.out.println("Valid");
+                        break;
+
+        case 5:
+            System.out.println("Valid");
+                        break;
+
+        case 6:
+            System.out.println("Valid");
+                        break;
+
+        case 7:
+            System.out.println("Valid");
+                        break;
+
+        case 8:
+            System.out.println("Valid");
+                        break;
+
+        case 9:
+            System.out.println("Valid");
+                        break;
+        default:
+            System.out.println("Invalid");
+                        break;
+
+            
+    }
+    
+    }
+    
+}
